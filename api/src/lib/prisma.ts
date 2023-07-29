06c19d6d26cb98170fa79d10b80e0ecde235945d
shoet <32211937+shoet@users.noreply.github.com>
2023-07-29 15:41:50 +0900

    SQL出力

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -4,7 +4,14 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'production'
+        ? ['error']
+        : ['query', 'info', 'warn', 'error'],
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
